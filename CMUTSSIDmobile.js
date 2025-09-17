bdffcbb4ed0effb7d142b15eb9e1d616eab4c584
asdmoment <67852913+asdmoment@users.noreply.github.com>
2025-09-17 18:48:18 +0800

    Add mobile support

---
+++ CMUTSSIDmobile.js
@@ -0,0 +1,20 @@
+// ssid.js
+function switchPolicyBySSID() {
+  // 获取当前 Wi-Fi 的 SSID
+  const currentSSID = $network.wifi.ssid;
+  const targetSSID = "CUMT_Stu"; 
+  const groupName = "CUMT"; 
+
+  if (currentSSID === targetSSID) {
+    $surge.setSelectGroupPolicy(groupName, "DIRECT");
+    console.log(`检测到 SSID: ${currentSSID}，策略组 '${groupName}' 已切换至 DIRECT`);
+  } else {
+    // 如果不是，就切换到 PROXY
+    $surge.setSelectGroupPolicy(groupName, "DEVICE:mbp");
+    console.log(`SSID: ${currentSSID}，策略组 '${groupName}' 已切换至 DEVICE:mbp`);
+  }
+}
+
+// 脚本执行入口
+switchPolicyBySSID();
+$done();
